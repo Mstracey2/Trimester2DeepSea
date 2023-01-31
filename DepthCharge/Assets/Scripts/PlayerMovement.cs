239735b106879f53a65d7dea8b5c06e9932f1da7
TheBillSansum <thegoldesh@gmail.com>
2023-01-31 12:22:40 +0000

    Worked on hanging cosmetics+ small fixes

--- DepthCharge/Assets/Scripts/PlayerMovement.cs
+++ DepthCharge/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody playerRB;
+    [SerializeField] private GameObject Inventory;
 
     private void Start()
     {
@@ -24,5 +25,16 @@ public class PlayerMovement : MonoBehaviour
         {
             playerRB.AddForce(new Vector3(0, y, 0) * 10);
         }
+
+
+    //    if(Input.GetKeyDown(Keycode.M))
+      //  {
+        //    Inventory.SetActive(true);
+        //}
+
+        //if(Input.GetKeyDown("N"))
+        //{
+          //  Inventory.SetActive(false);
+        //}
     }
 }
