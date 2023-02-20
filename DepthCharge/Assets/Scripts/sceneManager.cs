10fa55ba52407aec0d83ec740220a596ddcd3e9f
Ethan Callow <ethan@ark.me.uk>
2023-02-20 17:29:35 +0000

    Launch sequence ready

--- DepthCharge/Assets/Scripts/sceneManager.cs
+++ DepthCharge/Assets/Scripts/sceneManager.cs
@@ -7,10 +7,48 @@ using UnityEditor.UI;
 public class sceneManager : MonoBehaviour
 {
     public string Link = "https://forms.gle/3e5T895P6ky8nhXQA";
+    public GameObject mech;
+    public Transform waypoint1;
+    public Transform waypoint2;
+    public float launch = 0f;
+
+    private void Update()
+    {
+        // If launch has started, go to first waypoint
+        if (launch == 1f)
+        {
+            var step = 25f * Time.deltaTime;
+            mech.transform.position = Vector3.MoveTowards(mech.transform.position, waypoint1.position, step);
+
+            if (mech.transform.position == waypoint1.position)
+            {
+                launch = 2f;
+            }
+        }
+
+        // If first waypoint was hit, go to second waypoint, then start the game
+        if (launch == 2f)
+        {
+            var step = 35f * Time.deltaTime;
+            mech.transform.position = Vector3.MoveTowards(mech.transform.position, waypoint2.position, step);
+
+            if (mech.transform.position == waypoint2.position)
+            {
+                launch = 0f;
+                loadMain();
+            }
+        }
+    }
+
     public void playGame()
     {
-        SceneManager.LoadScene("Scene");
+        // Safety measurt to prevent the button from activating multiple times
+        if (launch == 0)
+        {
+            launch = 1f;
+        }
     }
+
     public void exitGame()
     {
         Application.Quit();
@@ -19,5 +57,11 @@ public class sceneManager : MonoBehaviour
     public void setToClipboard()
     {
         GUIUtility.systemCopyBuffer = Link;
+    }
+
+    // Load the main level
+    public void loadMain()
+    {
+        SceneManager.LoadScene("Scene");
     }
 }
