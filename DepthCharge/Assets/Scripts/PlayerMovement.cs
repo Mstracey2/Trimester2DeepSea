9eb75224a3d835fb21630489da323a4590fb92c8
Ethan Callow <ethan@ark.me.uk>
2023-04-21 01:06:14 +0100

    Thruster Particle effects added

--- DepthCharge/Assets/Scripts/PlayerMovement.cs
+++ DepthCharge/Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,11 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private GameObject Inventory;
     [SerializeField] private BlipScript radarBlip;
 
+    public GameObject rightThrust;
+    public GameObject leftThrust;
+    public GameObject upThrust;
+    public GameObject downThrust;
+
     private void Start()
     {
         playerRB = GetComponent<Rigidbody>();
@@ -19,6 +24,7 @@ public class PlayerMovement : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
         
+        // Directional movement
         if(x != 0)                          //if A or D is pressed, add a force in the correct direction
         {
             playerRB.AddForce(new Vector3(x, 0, 0) * 30);
@@ -27,6 +33,31 @@ public class PlayerMovement : MonoBehaviour
         {
             playerRB.AddForce(new Vector3(0, y, 0) * 30);
         }
+
+        // Thrusters off
+        rightThrust.SetActive(false);
+        leftThrust.SetActive(false);
+        upThrust.SetActive(false);
+        downThrust.SetActive(false);
+
+        // Activate thrusters with movement
+        if (x > 0)
+        {
+            rightThrust.SetActive(true);
+        }
+        if (x < 0)
+        {
+            leftThrust.SetActive(true);
+        }
+        if (y > 0)
+        {
+            upThrust.SetActive(true);
+        }
+        if (y < 0)
+        {
+            downThrust.SetActive(true);
+        }
+
 
         if (Input.GetKey(KeyCode.Space))    //if space is pressed, then add force in the direction the player is moving
         {
